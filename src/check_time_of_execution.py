fcd9d4ad4c78fbd68cab262c547e60aa459db2dc
Dawid <dawid.skreczko@gmail.com>
2019-07-04 10:33:51 +0200

    solution of chess as list: -declare start point -declare board dimension - default 8

--- src/check_time_of_execution.py
+++ src/check_time_of_execution.py
@@ -1,5 +1,6 @@
 import time
-BOARD = list(['ee']*6 for x in range(6))
+import sys
+BOARD = list(['ee']*5 for x in range(5))
 POSSIBLE_MOVES = (
 	(-2, -1),
 	(-2, 1),
@@ -10,7 +11,7 @@ POSSIBLE_MOVES = (
 	(1, -2),
 	(1, 2),)
 movements = 1
-TAB = 5
+
 start = time.time()
 
 
@@ -52,10 +53,10 @@ start = time.time()
 
 """ 		66.73857593536377 		 """
 
-# def check_chess(x, y, movements):
+# def check_chess(x, y, movements=1):
 # 	global BOARD, start
 # 	BOARD[x][y] = movements
-# 	if movements == 36:
+# 	if movements == 25:
 # 		end = time.time()
 # 		print(BOARD)
 # 		print(end - start)
@@ -65,16 +66,31 @@ start = time.time()
 # 		for item in allowed_moves:
 # 			check_chess(item[0], item[1], movements+1)
 # 	BOARD[x][y]='ee'
-#
+
+
+# def check_chess(x, y, movements=1):
+# 	global BOARD
+# 	if movements < 16:
+# 		BOARD[x][y] = movements
+# 		print(BOARD)
+# 		print(movements)
+# 		moves = possible_moves(x,y)
+# 		if moves:
+# 			for item in moves:
+# 				check_chess(item[0],item[1], movements+1)
+# 		BOARD[x][y] = 'ee'
+# 		# else:
+# 		# 	if movements<16:
+# 		# 		BOARD[x][y] = 'ee'
+# 		# 	return BOARD
 #
 # def main():
-# 	check_chess(0,0, 1)
-# 	print(BOARD)
+# 	check_chess(0,0)
 #
 # def possible_moves(x,y):
 # 	ALLOWED_MOVES = []
 # 	for item in POSSIBLE_MOVES:
-# 		if (item[0]+x>=0 and item[0]+x<6) and (item[1]+y>=0 and item[1]+y<6) and (BOARD[item[0]+x][item[1]+y]=='ee'):
+# 		if (item[0]+x>=0 and item[0]+x<4) and (item[1]+y>=0 and item[1]+y<4) and (BOARD[item[0]+x][item[1]+y]=='ee'):
 # 			ALLOWED_MOVES += [(item[0]+x,item[1]+y)]
 # 	return ALLOWED_MOVES
 #
@@ -83,28 +99,27 @@ start = time.time()
 
 """ 		57.07637858390808 		 """
 
-# def check_chess(x, y, movements):
-# 	global BOARD, start
-# 	BOARD[x][y] = movements
-# 	if movements == 36:
-# 		end = time.time()
-# 		print(BOARD)
-# 		print(end - start)
-# 		sys.exit()
-# 	for item in POSSIBLE_MOVES:
-# 		if possible_moves(item[0]+x, item[1]+y):
-# 			check_chess(item[0]+x, item[1]+y, movements+1)
-# 	BOARD[x][y]='ee'
-#
-#
-# def main():
-# 	check_chess(0,0, 1)
-# 	print(BOARD)
-#
-# def possible_moves(x,y):
-# 	return (x>=0 and x<6) and (y>=0 and y<6) and BOARD[x][y]=='ee'
-#
-# main()
+def check_chess(x, y, movements):
+	global BOARD, start
+	BOARD[x][y] = movements
+	if movements == 25:
+		end = time.time()
+		print(BOARD)
+		print(end - start)
+	for item in POSSIBLE_MOVES:
+		if possible_moves(item[0]+x, item[1]+y):
+			check_chess(item[0]+x, item[1]+y, movements+1)
+	BOARD[x][y]='ee'
+
+
+def main():
+	check_chess(0,0, 1)
+	print(BOARD)
+
+def possible_moves(x,y):
+	return (x>=0 and x<5) and (y>=0 and y<5) and BOARD[x][y]=='ee'
+
+main()
 
 """ 	60.15519666671753 		 """
 
---
+++ src/solving_chess.py
@@ -0,0 +1,45 @@
+BOARD = []
+POSSIBLE_MOVES = (
+	(-2, -1),
+	(-2, 1),
+	(2, -1),
+	(2, 1),
+	(-1, -2),
+	(-1, 2),
+	(1, -2),
+	(1, 2),)
+SOLID = 0
+
+def check_chess(x, y, movements=1):
+	global SOLID, BOARD
+	BOARD[x][y] = movements
+	if SOLID == 0:
+		if movements == len(BOARD)**2:
+			SOLID = 1						#needed for interrupting recursion after finding first solutoin
+		for item in POSSIBLE_MOVES:
+			if possible_moves(item[0]+x, item[1]+y):
+				check_chess(item[0]+x, item[1]+y, movements+1)
+		if SOLID ==0 and movements<len(BOARD)**2:
+			BOARD[x][y]='ee'
+
+
+
+def possible_moves(x,y):
+	return (x>=0 and x<len(BOARD)) and (y>=0 and y<len(BOARD)) and BOARD[x][y]=='ee'
+
+def get_chess_solution(x, y, board=8):
+	global BOARD
+	BOARD = list(['ee'] * board for x in range(board))
+	check_chess(x, y)
+	return BOARD
+
+
+
+
+
+
+
+
+
+
+
